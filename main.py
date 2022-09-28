9f50c24b226f972936abb5ff9446c2d114364ea9
Sabbha Mondal <113550518+sabbha6@users.noreply.github.com>
2022-09-28 16:51:06 +0000

    made changes to send keys in webdriver

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 
 def get_driver(url):
@@ -20,8 +24,23 @@ def get_driver(url):
 
 def main():
   driver = get_driver("https://automated.pythonanywhere.com/")
-  txt = driver.find_element(By.XPATH, "/html/body/div[1]/div/h1[1]")
-  print(txt.text)
+  time.sleep(2)
+  # click_login = driver.find_element(By.XPATH,
+  #                                   '//*[@id="basicExampleNav"]/div/a')
+  # click_login.click()
+  driver = get_driver("https://automated.pythonanywhere.com/login/")
+  username = driver.find_element(By.ID, 'id_username')
+  username.send_keys('automated')
+  password = driver.find_element(By.ID, 'id_password')
+  password.send_keys('automatedautomated' + Keys.RETURN)
+  time.sleep(2)
+  temperature = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/h1[2]')))
+  print(float(temperature.text.split(': ')[1]))
+  # print(driver.current_url)
 
+
+# txt = driver.find_element(By.XPATH, "/html/body/div[1]/div/h1[1]")
+# print(txt.text)
 
 main()
